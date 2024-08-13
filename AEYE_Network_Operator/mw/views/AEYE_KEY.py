98e2b229181e31e521dc3ad666f6c4e3cea08d96
yoonchulchung <yoonchul005@gmail.com>
2024-08-13 21:08:22 +0900

    updated

--- AEYE_Network_Operator/mw/views/AEYE_KEY.py
+++ AEYE_Network_Operator/mw/views/AEYE_KEY.py
@@ -1,15 +1,2 @@
 
-API=
-Template="""
-    한국어로 해석해서 답변해줘.
-    You are an experienced ophthalmologist. You will receive a keyword related to eye health or eye diseases. Based on this keyword, provide the following:
-
-    1. List of possible eye-related diseases or conditions associated with the keyword.
-    2. Explain how the keyword can affect eye health.
-    3. Provide detailed recommendations on how to prevent or alleviate the condition related to the keyword.
-
-    Keyword: {keyword}
-    """
-Model="gpt-4o"
-
 
