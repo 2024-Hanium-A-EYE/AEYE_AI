6a7cc7e215fd479195da3a015d5b0eda69aaae12
yoonchulchung <yoonchul005@gmail.com>
2024-08-16 23:14:09 +0900

    updated

---
+++ get_weight.sh
@@ -0,0 +1,35 @@
+#############################################################
+# AEYE AI Weight Fetcher
+# Created By Yoonchul Chung
+# Created At 2024.08.16
+# Welcome to Visit Github : https://github.com/Yoonchulchung
+#############################################################
+
+
+install_mega_cloud()
+{
+    apt-get update
+    apt-get install figlet
+
+    figlet install
+    figlet MEGA
+
+    apt-get install wget gnupg
+    wget -qO - https://mega.nz/keys/MEGA_signing.key | apt-key add -
+    echo "deb https://mega.nz/linux/repo/xUbuntu_$(lsb_release -rs) ./" | tee /etc/apt/sources.list.d/mega.list
+    apt-get update
+    apt-get install megacmd
+}
+
+get_weight_file()
+{
+    cd AEYE_Network_Operator/mw/views/weight && mega-get 'https://mega.nz/file/LVw0UahD#WOjbPdOBZ0RiR2wN-xJ42I8seW6x5FSQTjfTvwdxI5I'
+}
+
+run()
+{
+    install_mega_cloud
+    get_weight_file
+}
+
+run
