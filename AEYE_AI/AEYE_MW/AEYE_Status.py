4a668b443c0fef6a9ddd2a295cf11ca2e6efffd1
yoonchulchung <yoonchul005@gmail.com>
2024-08-17 22:36:27 +0900

    updated status

--- AEYE_AI/AEYE_MW/AEYE_Status.py
+++ AEYE_AI/AEYE_MW/AEYE_Status.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request, Blueprint
 from datetime import datetime
 from colorama import Fore, Back, Style
+import requests
 
 mw_status = Blueprint('AEYE_MW_Status', __name__)
 i_am_mw_status = 'OpticNet MW - Status'
@@ -32,22 +33,44 @@ def aeye_mw_status() :
 
     print_log('active', i_am_client, i_am_mw_status, message_client)
 
-    return jsonify({'Status' :"good"}), 200
-
-
-
-def check_valid_data(whoami, status, message) :
-
-    if whoami :
-        if status:
-            if message :
-                url = ''
+    if i_am_client :
+        if status_client:
+            if message_client :
+                url = 'http://127.0.0.1:2000/hal/status/'
+                print_log('active', i_am_client, i_am_mw_status, "requst data to : {}".format(url))
+                data={
+                    'whoami' : i_am_mw_status,
+                    'message': message_client,
+                    'status' : status_client
+                }
+                response = requests.post(url, data=data)
+                
+                if response.status_code == 200:
+                    message="Succed to send data to : {}".format(url)
+                    print_log('active', i_am_mw_status, i_am_mw_status, message)
+                    data={
+                        'whoami' : i_am_mw_status,
+                        'message': message
+                    }
+                    return jsonify(data), 200
+                else:
+                    message="Failed to send data to : {}".format(url)
+                    print_log('error', i_am_mw_status, i_am_mw_status, message)
+                    data={
+                        'whoami' : i_am_mw_status,
+                        'message': message
+                    }
+                    return jsonify(data), 400
             else :
-                print_log('error', whoami, mw_status, "Failed to Receive message")
+                print_log('error', i_am_mw_status, i_am_mw_status, "Failed to Receive message")
                 return 400
         else:
-            print_log('error', whoami, mw_status, "Failed to Receive status")
+            print_log('error', i_am_mw_status, i_am_mw_status, "Failed to Receive status")
             return 400
     else:
-        print_log('error', 'Client', mw_status, "Failed to Receive whoami")
+        print_log('error', i_am_client, i_am_mw_status, "Failed to Receive from : {}".format(i_am_client))
         return 400
+
+
+
+
